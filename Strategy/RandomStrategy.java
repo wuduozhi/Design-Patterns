42fe7d398c0081369dc4147464b347b68d7c2832
wuduozhi <wduozhi@gmail..com>
2018-12-15 14:32:06 +0800

    '添加多种设计模式'

---
+++ Strategy/RandomStrategy.java
@@ -0,0 +1,14 @@
+import java.util.Random;
+
+public class ProbStrategy implements Strategy {
+    private Random random;
+    public RandomStrategy(int seed) {
+        random = new Random(seed);
+    }
+    public Hand nextHand() {
+        return Hand.getHand(random.nextInt(3));
+    }
+    public void study(boolean win) {
+        
+    }
+}
